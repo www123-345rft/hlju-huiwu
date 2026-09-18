c6d766aa4ca463237f3d38fc059106c5faaea985
www123-345rft <a19846439086@qq.com>
2026-09-18 19:57:21 +0800

    Update docs/assets/js/cdn.js

--- docs/assets/js/cdn.js
+++ docs/assets/js/cdn.js
@@ -1,3 +1,10 @@
+var ASSET_VER = "20260918d";
+window.assetFallback = function (rel) {
+  return rel + (rel.indexOf("?") >= 0 ? "&" : "?") + "v=" + ASSET_VER;
+};
 window.assetUrl = function (rel) {
-  return rel + (rel.indexOf("?") >= 0 ? "&" : "?") + "v=20260918c";
+  var local = window.assetFallback(rel);
+  var host = location.hostname;
+  if (host === "127.0.0.1" || host === "localhost" || location.protocol === "file:") return local;
+  return "https://fastly.jsdelivr.net/gh/www123-345rft/hlju-huiwu@main/docs/" + rel + "?v=" + ASSET_VER;
 };
