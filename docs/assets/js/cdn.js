8758829b943c5e6d11aef9d0d52e1dfdf0d88ed7
www123-345rft <a19846439086@qq.com>
2026-09-18 19:29:33 +0800

    Add docs/assets/js/cdn.js

---
+++ docs/assets/js/cdn.js
@@ -0,0 +1,8 @@
+(function () {
+  const host = location.hostname;
+  const local = host === "127.0.0.1" || host === "localhost" || location.protocol === "file:";
+  window.assetUrl = function (rel) {
+    if (local) return rel;
+    return "https://cdn.jsdelivr.net/gh/www123-345rft/hlju-huiwu@main/docs/" + rel;
+  };
+})();
