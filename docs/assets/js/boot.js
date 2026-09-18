608f3ae291229ed0313c90bccae9ca823233ddd3
www123-345rft <a19846439086@qq.com>
2026-09-18 19:47:05 +0800

    Phone cache-bust assets/js/boot.js

---
+++ docs/assets/js/boot.js
@@ -0,0 +1,10 @@
+(function () {
+  var VER = "20260918b";
+  try {
+    var u = new URL(location.href);
+    if (u.searchParams.get("v") !== VER) {
+      u.searchParams.set("v", VER);
+      location.replace(u.href);
+    }
+  } catch (e) {}
+})();
